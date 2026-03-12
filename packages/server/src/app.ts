93b5b4dc79cbc039730c36b0a5658063bb7afb0a
VolodymyrKirichenko <valodya.kirik@gmail.com>
2026-03-12 15:40:12 +0200

    feat: enhance API documentation endpoint with Swagger UI integration

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,7 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import swaggerUi from 'swagger-ui-express';
 import {swaggerSpec} from './config/swagger';
 import eventsRouter from './routes/events';
 import authRouter from './routes/auth';
@@ -11,8 +10,25 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.get('/api/docs.json', (_req, res) => res.json(swaggerSpec));
+app.get('/api/docs', (_req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.send(`<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>Events API - Swagger</title>
+  <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css" />
+</head>
+<body>
+  <div id="swagger-ui"></div>
+  <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js"></script>
+  <script>
+    SwaggerUIBundle({ url: '/api/docs.json', dom_id: '#swagger-ui' });
+  </script>
+</body>
+</html>`);
+});
 
 app.use('/api/events', eventsRouter);
 app.use('/api/auth', authRouter);
